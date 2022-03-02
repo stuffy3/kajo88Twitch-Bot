ac9a04fb2f3925475a442bb78bb58390b1446c05
Tanner Stufflebeam <tannerstufflebeam@gmail.com>
2022-03-01 19:44:06 -0700

    added a loop of chat messages that spam the socials

--- Socials.js
+++ Socials.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 
 const tmi = require('tmi.js');
 
+let twitter = 'https://www.twitter.com/Kajo_ssb'
+let youtube = 'https://bit.ly/3viQgHw' 
+
 // Define configuration options
 const client = new tmi.Client({
   connection: {
@@ -14,31 +17,40 @@ const client = new tmi.Client({
     password: process.env.TWITCH_OAUTH_TOKEN
   },
 });
+function timeoutLoop(delay) { setTimeout(function () {
+  socialsLoop('#kajo88')
+}, delay)
+}
+
+function socialsLoop(channel) {
+  console.log('TimeoutLoop')
+  client.say(channel, ` SeemsGood Check out my socials! SeemsGood  Twitter: ${twitter}  Youtube: ${youtube}`)
+  timeoutLoop(900000)
+}
+
 
 client.on('connected', onConnectedHandler);
 
 client.connect();
 
 client.on('message', (channel, tags, message, self) => {
-    const isNotBot = tags.username.toLowerCase() !== process.env.TWITCH_BOT_USERNAME
-    let twitter = 'https://bit.ly/3plWkv6'
-    let youtube = 'https://bit.ly/3viQgHw' 
+    const isNotBot = tags.username !== process.env.TWITCH_BOT_USERNAME
    if (!isNotBot) {return}
 
     let commandName = message;
 
     if (commandName === '!socials') {
         console.log(channel, "Hello")
-        client.say(channel,`                                                               
+        client.say(channel, `                                                       
         Twitter: ${twitter}      
         Youtube: ${youtube}`)
-        setTimeout(10000)
+      
     } 
 });
 
 function onConnectedHandler (addr, port) {
-  client.say(channel, )
-    console.log(`* Connected to ${addr}:${port}`);
+  console.log(`* Connected to ${addr}:${port}`);
+   return timeoutLoop(1000)
 }
 
 
